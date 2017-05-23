17e7933aae0059170082eb8e5e4407b4be2de9dc
Tony Susi <tony.susi@99.co.nz>
2017-05-23 16:32:55 +1200

    5 form fields
    Trial form with validation
    Move questions into body
    remove phone number from form copy
    Contact us in header
    new video in header
    new header image

--- _site/js/contact_me.js
+++ _site/js/contact_me.js
@@ -1,70 +1,154 @@
-$(function() {
+$(function () {
 
-    $("input,textarea").jqBootstrapValidation({
+
+    $("input,select").jqBootstrapValidation({
         preventSubmit: true,
-        submitError: function($form, event, errors) {
+        submitError: function ($form, event, errors) {
             // additional error messages or events
         },
-        submitSuccess: function($form, event) {
+        submitSuccess: function ($form, event) {
             event.preventDefault(); // prevent default submit behaviour
             // get values from FORM
             var name = $("input#name").val();
             var email = $("input#email").val();
             var phone = $("input#phone").val();
-            var message = $("textarea#message").val();
-            var firstName = name; // For Success/Failure Message
-            // Check for white space in name for Success/Fail message
-            if (firstName.indexOf(' ') >= 0) {
-                firstName = name.split(' ').slice(0, -1).join(' ');
+            var demand = $("input[name=demandOptions]:checked").val();
+            var size = $("select[name=parkingSpaces] option:selected").val();
+            var valid = true;
+            // validate name
+            if (name === "") {
+                valid = false;
+                $("input#name").parent().addClass('has-error');
+                $("input#name").next().text($("input#name").data('validation-required-message'));
+            } else {
+                $("input#name").parent().removeClass('has-error');
+                $("input#name").next().text('');
             }
-            $.ajax({
-                url: "././mail/contact_me.php",
-                type: "POST",
-                data: {
-                    name: name,
-                    phone: phone,
-                    email: email,
-                    message: message
-                },
-                cache: false,
-                success: function() {
-                    // Success message
-                    $('#success').html("<div class='alert alert-success'>");
-                    $('#success > .alert-success').html("<button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;")
-                        .append("</button>");
-                    $('#success > .alert-success')
-                        .append("<strong>Your message has been sent. </strong>");
-                    $('#success > .alert-success')
-                        .append('</div>');
+            // validate email
+            if (email === "") {
+                valid = false;
+                $("input#email").parent().addClass('has-error');
+                $("input#email").next().text($("input#email").data('validation-required-message'));
+            } else {
+                $("input#email").parent().removeClass('has-error');
+                $("input#email").next().text('');
+            }
+            // validate phone
+            if (phone === "") {
+                valid = false;
+                $("input#phone").parent().addClass('has-error');
+                $("input#phone").next().text($("input#phone").data('validation-required-message'));
+            } else {
+                $("input#phone").parent().removeClass('has-error');
+                $("input#phone").next().text('');
+            }
+            // validate demand
+            if (!demand) {
+                valid = false;
+                $("input#demandYes").parent().parent().parent().find("p.help-block").text($("input#demandYes").data('validation-required-message'));
+            } else {
+                $("input#demandYes").parent().parent().parent().find("p.help-block").text('');
+            }
+            // validate size
+            if (size === "") {
+                valid = false;
+                $("select[name=parkingSpaces]").parent().parent().find("p.help-block").text($("select[name=parkingSpaces]").data('validation-required-message'));
+            } else {
+                $("select[name=parkingSpaces]").parent().parent().find("p.help-block").text('');
+            }
 
-                    //clear all fields
-                    $('#contactForm').trigger("reset");
-                },
-                error: function() {
-                    // Fail message
-                    $('#success').html("<div class='alert alert-danger'>");
-                    $('#success > .alert-danger').html("<button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;")
-                        .append("</button>");
-                    $('#success > .alert-danger').append("<strong>Sorry " + firstName + ", it seems that my mail server is not responding. Please try again later!");
-                    $('#success > .alert-danger').append('</div>');
-                    //clear all fields
-                    $('#contactForm').trigger("reset");
-                },
-            })
+            if (valid) {
+                $.ajax({
+                    url: "https://formspree.io/xeeeyzqx",
+                    //                url: "https://formspree.io/tonysjunk@gmail.com",
+                    method: "POST",
+                    dataType: "json",
+                    data: {
+                        name: name,
+                        phone: phone,
+                        email: email,
+                        demand: demand,
+                        size: size
+                    },
+                    cache: false,
+                    success: function () {
+                        // Success message
+                        $('#success').html("<div class='alert alert-success'>");
+                        $('#success > .alert-success').html("<button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;").append("</button>");
+                        $('#success > .alert-success').append("<strong>Your message has been sent. </strong>");
+                        $('#success > .alert-success').append('</div>');
+                        //clear all fields
+                        $('#freeTrial').trigger("reset");
+                    },
+                    error: function () {
+                        // Fail message
+                        $('#success').html("<div class='alert alert-danger'>");
+                        $('#success > .alert-danger').html("<button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;").append("</button>");
+                        $('#success > .alert-danger').append("<strong>Sorry, it seems that my mail server is not responding. Please try again later!");
+                        $('#success > .alert-danger').append('</div>');
+                        //clear all fields
+                        $('#freeTrial').trigger("reset");
+                    },
+                })
+            }
         },
-        filter: function() {
+
+        filter: function () {
             return $(this).is(":visible");
         },
     });
+    /*
+    $('#freeTrial button').click(function (e) {
+        e.preventDefault(); // prevent default submit behaviour
+        console.log('submit form');
+        // get values from FORM
+        var name = $("input#name").val();
+        var email = $("input#email").val();
+        var phone = $("input#phone").val();
+        var demand = $("input[name=demandOptions]:checked").val();
+        var size = $("select[name=parkingSpaces] option:selected").val();
 
-    $("a[data-toggle=\"tab\"]").click(function(e) {
+
+        $.ajax({
+            url: "https://formspree.io/xeeeyzqx",
+            //                url: "https://formspree.io/tonysjunk@gmail.com",
+            method: "POST",
+            dataType: "json",
+            data: {
+                name: name,
+                phone: phone,
+                email: email,
+                demand: demand,
+                size: size
+            },
+            cache: false,
+            success: function () {
+                // Success message
+                $('#success').html("<div class='alert alert-success'>");
+                $('#success > .alert-success').html("<button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;").append("</button>");
+                $('#success > .alert-success').append("<strong>Your message has been sent. </strong>");
+                $('#success > .alert-success').append('</div>');
+                //clear all fields
+                $('#contactForm').trigger("reset");
+            },
+            error: function () {
+                // Fail message
+                $('#success').html("<div class='alert alert-danger'>");
+                $('#success > .alert-danger').html("<button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;").append("</button>");
+                $('#success > .alert-danger').append("<strong>Sorry, it seems that my mail server is not responding. Please try again later!");
+                $('#success > .alert-danger').append('</div>');
+                //clear all fields
+                $('#freeTrial').trigger("reset");
+            },
+        })
+    });
+    */
+    $("a[data-toggle=\"tab\"]").click(function (e) {
         e.preventDefault();
         $(this).tab("show");
     });
 });
-
-
 /*When clicking on Full hide fail/success boxes */
-$('#name').focus(function() {
+$('#name').focus(function () {
     $('#success').html('');
 });
