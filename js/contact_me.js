b843ec9ce463fbbd2db1014c26e3e079884e94b9
Tony Susi <tony.susi@99.co.nz>
2017-05-26 15:38:42 +1200

    removed commented out section

--- js/contact_me.js
+++ js/contact_me.js
@@ -105,52 +105,7 @@ $(function () {
             return $(this).is(":visible");
         },
     });
-    /*
-    $('#freeTrial button').click(function (e) {
-        e.preventDefault(); // prevent default submit behaviour
-        console.log('submit form');
-        // get values from FORM
-        var name = $("input#name").val();
-        var email = $("input#email").val();
-        var phone = $("input#phone").val();
-        var demand = $("input[name=demandOptions]:checked").val();
-        var size = $("select[name=parkingSpaces] option:selected").val();
 
-
-        $.ajax({
-            url: "https://formspree.io/xeeeyzqx",
-            //                url: "https://formspree.io/tonysjunk@gmail.com",
-            method: "POST",
-            dataType: "json",
-            data: {
-                name: name,
-                phone: phone,
-                email: email,
-                demand: demand,
-                size: size
-            },
-            cache: false,
-            success: function () {
-                // Success message
-                $('#success').html("<div class='alert alert-success'>");
-                $('#success > .alert-success').html("<button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;").append("</button>");
-                $('#success > .alert-success').append("<strong>Your message has been sent. </strong>");
-                $('#success > .alert-success').append('</div>');
-                //clear all fields
-                $('#contactForm').trigger("reset");
-            },
-            error: function () {
-                // Fail message
-                $('#success').html("<div class='alert alert-danger'>");
-                $('#success > .alert-danger').html("<button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;").append("</button>");
-                $('#success > .alert-danger').append("<strong>Sorry, it seems that my mail server is not responding. Please try again later!");
-                $('#success > .alert-danger').append('</div>');
-                //clear all fields
-                $('#freeTrial').trigger("reset");
-            },
-        })
-    });
-    */
     $("a[data-toggle=\"tab\"]").click(function (e) {
         e.preventDefault();
         $(this).tab("show");
